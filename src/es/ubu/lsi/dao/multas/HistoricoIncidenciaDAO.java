92cf140e1dbe29a96f6d082564110a8349567235
Karima <kdr1001@alu.ubu.es>
2025-06-11 18:06:01 +0200

    Implementación de HistoricoIncidenciaDAO para trazabilidad de multas

---
+++ src/es/ubu/lsi/dao/multas/HistoricoIncidenciaDAO.java
@@ -0,0 +1,22 @@
+package es.ubu.lsi.dao.multas;
+
+import java.util.List;
+import javax.persistence.TypedQuery;
+import es.ubu.lsi.dao.JpaDAO;
+import es.ubu.lsi.model.multas.HistoricoIncidencia;
+import es.ubu.lsi.model.multas.IncidenciaPK;
+
+public class HistoricoIncidenciaDAO extends JpaDAO<HistoricoIncidencia, IncidenciaPK> {
+
+    @Override
+    public List<HistoricoIncidencia> findAll() {
+        return getEntityManager().createQuery("SELECT h FROM HistoricoIncidencia h", HistoricoIncidencia.class).getResultList();
+    }
+    
+    public List<HistoricoIncidencia> findByNif(String nif) {
+        TypedQuery<HistoricoIncidencia> query = getEntityManager().createQuery(
+            "SELECT h FROM HistoricoIncidencia h WHERE h.conductor.nif = :nif", HistoricoIncidencia.class);
+        query.setParameter("nif", nif);
+        return query.getResultList();
+    }
+} 
