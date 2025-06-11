9c934c44c898e941231589fb15b2a4eaa5ffd70e
Karima <kdr1001@alu.ubu.es>
2025-06-11 16:51:43 +0200

    Creación de clase embebible DireccionPostal para gestión de direcciones

---
+++ src/es/ubu/lsi/model/multas/DireccionPostal.java
@@ -0,0 +1,93 @@
+package es.ubu.lsi.model.multas;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class DireccionPostal {
+    private String direccion;
+    private String codigoPostal;
+    private String ciudad;
+
+    public DireccionPostal() {
+    }
+
+    public DireccionPostal(String direccion, String codigoPostal, String ciudad) {
+        this.direccion = direccion;
+        this.codigoPostal = codigoPostal;
+        this.ciudad = ciudad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    @Override
+    public String toString() {
+        return "DireccionPostal [direccion=" + direccion + ", codigoPostal=" + codigoPostal + ", ciudad=" + ciudad + "]";
+    }
+} import javax.persistence.Embeddable;
+
+@Embeddable
+public class DireccionPostal {
+    private String direccion;
+    private String codigoPostal;
+    private String ciudad;
+
+    public DireccionPostal() {
+    }
+
+    public DireccionPostal(String direccion, String codigoPostal, String ciudad) {
+        this.direccion = direccion;
+        this.codigoPostal = codigoPostal;
+        this.ciudad = ciudad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    @Override
+    public String toString() {
+        return "DireccionPostal [direccion=" + direccion + ", codigoPostal=" + codigoPostal + ", ciudad=" + ciudad + "]";
+    }
+} 
