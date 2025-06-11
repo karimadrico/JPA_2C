f524c53d817c21df50a929b351acb6acdaea934c
Karima <kdr1001@alu.ubu.es>
2025-06-11 18:02:55 +0200

    Implementación de VehiculoDAO para administración de vehículos

---
+++ src/es/ubu/lsi/dao/multas/VehiculoDAO.java
@@ -0,0 +1,29 @@
+package es.ubu.lsi.dao.multas;
+
+import java.util.List;
+import javax.persistence.EntityGraph;
+import javax.persistence.TypedQuery;
+import es.ubu.lsi.dao.JpaDAO;
+import es.ubu.lsi.model.multas.Vehiculo;
+
+public class VehiculoDAO extends JpaDAO<Vehiculo, String> {
+
+    @Override
+    public List<Vehiculo> findAll() {
+        return getEntityManager().createQuery("SELECT v FROM Vehiculo v", Vehiculo.class).getResultList();
+    }
+    
+    public List<Vehiculo> findAllWithGraph() {
+        EntityGraph<Vehiculo> graph = getEntityManager().createEntityGraph(Vehiculo.class);
+        graph.addAttributeNodes("conductores");
+        graph.addSubgraph("conductores").addAttributeNodes("incidencias", "historicos");
+        graph.addSubgraph("conductores").addSubgraph("incidencias").addAttributeNodes("tipoIncidencia");
+        graph.addSubgraph("conductores").addSubgraph("historicos").addAttributeNodes("tipoIncidencia");
+        
+        TypedQuery<Vehiculo> query = getEntityManager().createQuery(
+            "SELECT DISTINCT v FROM Vehiculo v LEFT JOIN FETCH v.conductores", Vehiculo.class);
+        query.setHint("javax.persistence.fetchgraph", graph);
+        
+        return query.getResultList();
+    }
+} 
