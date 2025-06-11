a71b0c6bfde3ab5f66c7a9f94d9b7f760065b201
Karima <kdr1001@alu.ubu.es>
2025-06-11 18:05:11 +0200

    Desarrollo de IncidenciaDAO para gestión principal de multas

---
+++ src/es/ubu/lsi/dao/multas/IncidenciaDAO.java
@@ -0,0 +1,38 @@
+package es.ubu.lsi.dao.multas;
+
+import java.util.Date;
+import java.util.List;
+import javax.persistence.TypedQuery;
+import es.ubu.lsi.dao.JpaDAO;
+import es.ubu.lsi.model.multas.Incidencia;
+import es.ubu.lsi.model.multas.IncidenciaPK;
+
+public class IncidenciaDAO extends JpaDAO<Incidencia, IncidenciaPK> {
+
+    @Override
+    public List<Incidencia> findAll() {
+        return getEntityManager().createQuery("SELECT i FROM Incidencia i", Incidencia.class).getResultList();
+    }
+    
+    public List<Incidencia> findByNif(String nif) {
+        TypedQuery<Incidencia> query = getEntityManager().createQuery(
+            "SELECT i FROM Incidencia i WHERE i.conductor.nif = :nif", Incidencia.class);
+        query.setParameter("nif", nif);
+        return query.getResultList();
+    }
+    
+    public void deleteByNif(String nif) {
+        getEntityManager().createQuery(
+            "DELETE FROM Incidencia i WHERE i.conductor.nif = :nif")
+            .setParameter("nif", nif)
+            .executeUpdate();
+    }
+    
+    public Incidencia create(Date fecha, String nif, String anotacion) {
+        Incidencia incidencia = new Incidencia();
+        incidencia.setId(new IncidenciaPK(fecha, nif));
+        incidencia.setAnotacion(anotacion);
+        getEntityManager().persist(incidencia);
+        return incidencia;
+    }
+} 
